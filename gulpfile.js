22a41a699626f80885f19847cfaae4c16e1cc688
Nobhpadol Boonphet <spritelemon36@hotmail.com>
2018-11-20 13:39:25 +0700

    myBooking page

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,45 @@
-let gulp = require('gulp');
-let sass = require('gulp-sass');
-let concat = require('gulp-concat')
-let cleanCSS = require('gulp-clean-css');
-let gls = require('gulp-live-server');
+let gulp = require("gulp");
+let sass = require("gulp-sass");
+let concat = require("gulp-concat");
+let cleanCSS = require("gulp-clean-css");
+let gls = require("gulp-live-server");
 
-gulp.task('sass', function() {
-    gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'))
+gulp.task("sass", function() {
+  gulp
+    .src("sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./css/"));
 });
 
-gulp.task('minify-css', () => {
-    return gulp.src('css/**/*.css')
-      .pipe(cleanCSS({debug: true}, (details) => {
+gulp.task("minify-css", () => {
+  return gulp
+    .src("css/**/*.css")
+    .pipe(
+      cleanCSS({ debug: true }, details => {
         console.log(`${details.name}: ${details.stats.originalSize}`);
         console.log(`${details.name}: ${details.stats.minifiedSize}`);
-      }))
-    .pipe(concat('dist/min.css'))
-    .pipe(gulp.dest('./'));
-  });
-
-//Watch task
-gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['sass']);
-    gulp.watch('css/**/*.css',['minify-css']);
+      })
+    )
+    .pipe(concat("dist/min.css"))
+    .pipe(gulp.dest("./"));
 });
 
-gulp.task('serve', function() {
+//Watch task
+gulp.task("default", function() {
+  gulp.watch("sass/**/*.scss", ["sass"]);
+  gulp.watch("css/**/*.css", ["minify-css"]);
+});
+
+gulp.task("serve", function() {
   //1. serve with default settings
-  var server = gls.static(['dist'], process.env.PORT || 3000); //equals to gls.static('public', 3000);
+  var server = gls.static(["dist"], process.env.PORT || 3000); //equals to gls.static('public', 3000);
   server.start();
 
   //use gulp.watch to trigger server actions(notify, start or stop)
-  gulp.watch('sass/**/*.scss',['sass'], function (file) {
+  gulp.watch("sass/**/*.scss", ["sass"], function(file) {
     server.notify.apply(server, [file]);
   });
-  gulp.watch('css/**/*.css',['minify-css'], function (file) {
+  gulp.watch("css/**/*.css", ["minify-css"], function(file) {
     server.notify.apply(server, [file]);
   });
-
 });
